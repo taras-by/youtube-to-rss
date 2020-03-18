3556cc3c02b420e6d2cd8ff3ea114fb063a02227
Taras <incom@tut.by>
2020-03-18 20:33:18 +0300

    Refacor App\Core\Application class

--- app/Core/Application.php
+++ app/Core/Application.php
@@ -10,19 +10,15 @@ use DI\NotFoundException;
 class Application
 {
     protected $container;
-    protected $request;
-    protected $response;
     protected $router;
     protected $view;
 
     public function __construct(
         Container $container,
-        Response $response,
         Router $router,
         View $view
     )
     {
-        $this->response = $response;
         $this->router = $router;
         $this->container = $container;
         $this->view = $view;
@@ -34,8 +30,7 @@ class Application
             $controllerClassName = $this->router->getController();
             $action = $this->router->getAction();
         } catch (\Exception $exception) {
-            $this->sendNotFound();
-            return;
+            $this->notFoundResponse()->send();
         }
 
         try {
@@ -44,17 +39,13 @@ class Application
             $controllerObject->setContainer($this->container);
             $response = $this->container->call([$controllerObject, $action]);
         } catch (DependencyException $e) {
-            $this->sendNotFound($e->getMessage());
-            return;
+            $response = $this->notFoundResponse($e->getMessage());
         } catch (NotFoundException $e) {
-            $this->sendNotFound($e->getMessage());
-            return;
+            $response = $this->notFoundResponse($e->getMessage());
         } catch (NotFoundHttpException $e) {
-            $this->sendNotFound($e->getMessage());
-            return;
+            $response = $this->notFoundResponse($e->getMessage());
         } catch (\Throwable $e) {
-            $this->sendError($e);
-            return;
+            $response = $this->errorResponse($e);
         }
 
         if ($response instanceof Response) {
@@ -62,27 +53,19 @@ class Application
         }
     }
 
-    private function sendNotFound(string $message = null)
-    {
-        $this->notFoundResponse($message)->send();
-    }
-
     public function notFoundResponse(string $message = null): Response
     {
-        $this->response->setHeader('HTTP/1.0 404 Not Found');
         $content = $this->view->render('error.404', ['message' => $message]);
-        return $this->response->setBody($content);
-    }
-
-    private function sendError(\Throwable $exception)
-    {
-        $this->errorResponse($exception)->send();
+        return (new Response())
+            ->setHeader('HTTP/1.0 404 Not Found')
+            ->setBody($content);
     }
 
     public function errorResponse(\Throwable $exception): Response
     {
-        $this->response->setHeader('HTTP/1.0 500 Internal Server Error');
         $content = $this->view->render('error.500', ['exception' => $exception]);
-        return $this->response->setBody($content);
+        return (new Response())
+            ->setHeader('HTTP/1.0 500 Internal Server Error')
+            ->setBody($content);
     }
 }
